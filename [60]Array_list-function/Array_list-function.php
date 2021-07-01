89556f964dedf6657200833b24631e24b1aa8c53
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-01 22:19:34 +0600

    video-Number: [60] Project-Name: Array_list-function

---
+++ [60]Array_list-function/Array_list-function.php
@@ -0,0 +1,36 @@
+<?php
+
+$color=array("red","green","yellow","pink");
+list($a,$b,$c,$d) = $color;
+echo "The color is : $a <br>";
+echo "The color is : $b <br>";
+echo "The color is : $c <br>";
+echo "The color is : $d <br>";
+
+
+
+$animal=array("fox","tiger","lion","elephant","birds");
+list($a,$b,$c,$d,$e) = $animal;
+echo "This is a : $a <br>";
+echo "This is a : $b <br>";
+echo "This is a : $c <br>";
+echo "This is a : $d <br>";
+echo "This is a : $e <br>";
+
+
+$number=array(5,45,10,94,23);
+list($a,$b,$c,$d,$e) = $number;
+echo "The number is : $a <br>";
+echo "The number is : $b <br>";
+echo "The number is : $c <br>";
+echo "The number is : $d <br>";
+echo "The number is : $e <br>";
+
+$color=array(0 => "red",1 => "green",2 => "yellow",3 => "pink");
+list($a,$b,$c,$d) = $color;
+echo "The color is : $a <br>";
+echo "The color is : $b <br>";
+echo "The color is : $c <br>";
+echo "The color is : $d <br>";
+
+?>
