86dd4b2df266977a463ba6dd546b8ddcc23f2f4e
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-02 20:42:41 +0600

    Video-Number:  [67] Project-Name:  String_search_Find-position-function

---
+++ [67]String_search_Find-position-function/String_search_Find-position-function.php
@@ -0,0 +1,31 @@
+<?php
+
+$name="the world. The world is so big";
+echo strpos($name,"world");
+
+$name="this is a home.the home is so beautiful";
+echo strpos($name,"home",20);
+
+
+
+
+ $animal="the lion in a forest respect lion others to a king";
+ echo strrpos($animal,"lion");
+
+$bird="the parrot is a beautiful parot";
+echo strrpos($bird,"parrot");
+
+
+
+
+
+$name="the world. The world is so big";
+echo stripos($name,"world");
+
+$name="this is a home.the home is so beautiful";
+echo stripos($name,"home",15);
+
+
+
+
+?>
