d18067168cb32057642e79b431fa22b094064c5e
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-02 22:14:08 +0600

    Video_Number:  [69] Project-Name:  String_substr-Function

---
+++ [69]String_substr-Function/String_substr-Function.php
@@ -0,0 +1,17 @@
+<?php
+
+$name="This is a name.The name is so nice";
+echo substr($name,5)."<br>";
+
+$animal="this is a cat.The cat is so cute";
+echo substr($animal,10)."<br>";
+
+
+$animal="This a cow.The cow is so hungry";
+echo substr("$animal",5,19)."<br>";
+
+$animal="This is a parrot.its look so beautiful";
+echo substr($animal,-20)."<br>";
+
+$animal="THis is my home.i always liver there";
+echo substr($animal,-20,16)."<br>";
