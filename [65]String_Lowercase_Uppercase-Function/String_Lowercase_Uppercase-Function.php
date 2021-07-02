cb9e7a00bc8929acdeded6b51517bd965e25174f
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-02 12:46:47 +0600

    Video-Number:  [65] Project-Name:  String_Lowercase_Uppercase-Function

---
+++ [65]String_Lowercase_Uppercase-Function/String_Lowercase_Uppercase-Function.php
@@ -0,0 +1,77 @@
+<?php
+
+$name="NISHAT";
+$new=strtolower($name);
+echo $new ."<br>";
+
+$name="ELEPHANT";
+$new=strtolower($name);
+echo $new . "<br>";
+
+$animal="LION";
+$new=strtolower($animal);
+echo $new;
+
+
+
+
+
+$name="nishat";
+$new=strtoupper($name);
+echo $new . "<br>";
+
+$animal="croko-dile";
+$new=strtoupper($animal);
+echo $new . "<br>";
+
+$animal="elephant";
+$new=strtoupper($animal);
+echo $new . "<br>";
+
+
+
+
+
+$name="Nishat";
+$new=lcfirst($name);
+echo $new . "<br>";
+
+$animal="Elephant";
+$new=lcfirst($animal);
+echo $new . "<br>";
+
+$animal="Tiger";
+$new=lcfirst($animal);
+echo $new . "<br>";
+
+
+
+
+$name="nishat";
+$new=ucfirst($name);
+echo $new . "<br>";
+
+$animal="lion";
+$new=ucfirst($animal);
+echo $new . "<br>";
+
+$animal="elephant";
+$new=ucfirst($animal);
+echo $new;
+
+
+
+
+
+$name="tanvirul hq";
+$new=ucwords($name);
+echo $new . "<br>";
+
+$name="amar desh";
+$new=ucwords($name);
+echo $new . "<br>";
+
+$animal="royel bengol tiger";
+$new=ucwords($animal);
+echo $new;
+?>
