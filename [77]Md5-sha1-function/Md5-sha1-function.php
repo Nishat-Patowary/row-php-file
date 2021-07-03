044169d84e3047b4730eb3217d9ac283a4df8361
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-03 22:11:28 +0600

    Video-number:  [77] Project-Name:  Md5-sha1-function

---
+++ [77]Md5-sha1-function/Md5-sha1-function.php
@@ -0,0 +1,51 @@
+<?php
+
+$name="nishat";
+echo md5($name)."<br>";
+
+$name="tanvirul HQ NIshat";
+echo md5($name)."<br>";
+
+$name="tanvirul";
+echo md5($name)."<br>"; 
+
+
+
+$name="Niloy";
+echo md5($name,true)."<br>";
+
+$name="mahmudul hasan";
+echo md5($name,true)."<br>";
+
+$name="tanvirul";
+echo md5($name,true)."<br>";
+
+
+
+//md5 end--------------
+
+
+$name="nishat";
+echo sha1($name)."<br>";
+
+$name="tanvirul Hq nishat";
+echo sha1($name)."<br>";
+
+$name="tanvir";
+echo sha1($name)."<br>";
+
+
+
+
+$name="nishat";
+echo sha1($name,true)."<br>";
+
+$name="tanvirul hq nishat";
+echo sha1($name,true)."<br>";
+
+$name="tanvirul";
+echo sha1($name,true)."<br>";
+
+//sha1 end-----------
+
+?>
