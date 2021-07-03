09ef3fafc965350399cc85fa90be0ebdb0d126bb
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-03 10:23:49 +0600

    Video-Number:  [72] Project-Name:  String_strrev-str_shuffke-function

---
+++ [72]String_strrev-str_shuffke-function/String_strrev-str_shuffke-function.php
@@ -0,0 +1,17 @@
+<?php
+
+echo strrev("Nishat")."<br>";
+echo strrev("Tanvirul-HQ-Nishat")."<br>";
+echo strrev("this is a cow")."<br>";
+echo strrev("This is a bike")."<br>";
+echo strrev("This is a bird")."<br>";
+
+//strrev end-------
+
+echo str_shuffle("Nishat")."<br>";
+echo str_shuffle("Tanvirul-HQ-Nishat")."<br>";
+echo str_shuffle("THis is a cow")."<br>";
+echo str_shuffle("This is a cat")."<br>";
+echo str_shuffle("This is a bird")."<br>";
+
+?>
