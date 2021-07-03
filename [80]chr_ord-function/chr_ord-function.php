01758a077060e7bb07c012f0e347bbbab0b02819
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-03 23:25:52 +0600

    Video-Number:  [80] Project-Name;  chr_ord-function

---
+++ [80]chr_ord-function/chr_ord-function.php
@@ -0,0 +1,52 @@
+<?php
+
+$number=chr(54);
+echo $number;
+
+$number=chr(51);
+echo $number;
+
+$number=chr(66);
+echo $number;
+
+$number=chr(58);
+echo $number;
+
+$number=chr(23);
+echo $number;
+
+$number=chr(0152);
+echo $number;
+
+$number=chr(2156);
+echo $number;
+
+$number=chr(1423);
+echo $number;
+
+$number=chr(2041);
+echo $number;
+
+$number=chr(0125);
+echo $number;
+//used by characters value---------
+//chr end-----------
+
+
+$name=ord("A");
+echo $name;
+
+$name=ord("y");
+echo $name;
+
+$name=ord("er");
+echo $name;
+
+$name=ord("i");
+echo $name;
+
+$name=ord("oi");
+echo $name;
+
+//ord end------------
+?>
