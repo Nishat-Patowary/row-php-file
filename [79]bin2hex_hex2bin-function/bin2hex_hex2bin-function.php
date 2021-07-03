77b992043b193550f4826fef19d790b0bdcbd996
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-03 22:55:30 +0600

    Video-Number:  [79] Project-Name:  bin2hex_hex2bin-function

---
+++ [79]bin2hex_hex2bin-function/bin2hex_hex2bin-function.php
@@ -0,0 +1,21 @@
+<?php
+
+echo bin2hex("nishat")."<br>";
+echo bin2hex("farhan tancir nishat")."<br>";
+echo bin2hex("tanvirul haque nishat")."<br>";
+//uses by security-----
+//bin2hex hexadecimal from--------------
+
+
+$name=bin2hex("tanvirul haque")."<br>";
+echo $name;
+
+echo hex2bin($name);
+
+
+$animal=bin2hex("the lion")."<br>";
+echo $animal;
+
+echo hex2bin($animal);
+
+?>
