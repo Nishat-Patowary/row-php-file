12d90f3e74675f66ff30b98847da81f5d7f2ed1d
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-04 21:39:38 +0600

    Video-Number:  [84] Project-Name:  Math-intdiv_sqrt_pow-function

---
+++ [84]Math-intdiv_sqrt_pow-function/Math-intdiv_sqrt_pow-function.php
@@ -0,0 +1,25 @@
+<?php 
+
+echo intdiv(5,2)."<br>";
+echo intdiv(4,2)."<br>";
+echo intdiv(9,2)."<br>";
+echo intdiv(12,4)."<br>";
+
+//intdiv function end---------
+
+echo sqrt(4)."<br>";
+echo sqrt(5)."<br>";
+echo sqrt(12)."<br>";
+echo sqrt(6)."<br>";
+echo sqrt(9)."<br>";
+
+//sqrt function end----------
+
+echo pow(4,4)."<br>";
+echo pow(2,4)."<br>";
+echo pow(8,2)."<br>";
+echo pow(10,2)."<br>"
+
+
+//pow function end---------
+?>
