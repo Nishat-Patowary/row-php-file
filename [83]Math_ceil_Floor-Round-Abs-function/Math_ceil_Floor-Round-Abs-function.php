5f42c672c1e9707f62494ba441c1fbb77b22866e
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-04 21:17:13 +0600

    Video-Number:  [83] Project-Name:  Math_ceil_Floor-Round-Abs-function

---
+++ [83]Math_ceil_Floor-Round-Abs-function/Math_ceil_Floor-Round-Abs-function.php
@@ -0,0 +1,49 @@
+<?php 
+
+echo ceil(0.5);
+echo ceil(0.81);
+echo ceil(-0.6);
+echo ceil(1.5);
+echo ceil(5.8);
+echo ceil(9.1);
+
+//ceil function end-----------
+
+echo floor(0.5);
+echo floor(0.68);
+echo floor(-5.4);
+echo floor(5.8);
+echo floor(9.4);
+echo floor(6.5);
+
+//floor function end--------------
+
+echo round(0.5);
+echo round(0.98);
+echo round(5.6);
+echo round(2.8);
+echo round(1.7);
+echo round(10.6);
+
+//round function end------------
+
+echo abs(0.5);
+echo abs(0.89);
+echo abs(1.5);
+echo abs(5.7);
+echo abs(-8.3);
+echo abs(-3.6);
+
+//abs function end-----------
+
+
+echo ceil(2.5);
+echo ceil(5.7);
+echo ceil(4.2);
+echo ceil(2.1);
+echo ceil(9.8);
+
+//ceil function end----------
+
+
+?>
