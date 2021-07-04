51391bf7e0b7ec855045087a34be40e601578299
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-04 21:58:38 +0600

    Video-Number:  [85] Project-Name:  Rand-mt_rand-lcg_value-function

---
+++ [85]Rand-mt_rand-lcg_value-function/Rand-mt_rand-lcg_value-function.php
@@ -0,0 +1,33 @@
+<?php 
+
+echo rand()."<br>";
+echo rand()."<br>";
+echo rand()."<br>";
+
+echo rand(0,10)."<br>";
+echo rand(5,15)."<br>";
+echo rand(50,84)."<br>";
+echo rand(15,20)."<br>";
+
+//rand function end-----------
+
+
+echo mt_rand()."<br>";
+echo mt_rand()."<br>";
+echo mt_rand()."<br>";
+
+echo mt_rand(0,10)."<br>";
+echo mt_rand(5,15)."<br>";
+echo mt_rand(50,84)."<br>";
+echo mt_rand(15,20)."<br>";
+
+//mt_rand function end----------
+
+
+echo lcg_value()."<br>";
+echo lcg_value()."<br>";
+echo lcg_value()."<br>";
+echo lcg_value()."<br>";
+
+//lcg_value function end-----------
+?>
