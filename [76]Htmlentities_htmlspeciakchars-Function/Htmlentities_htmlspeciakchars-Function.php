aeeb3d2b461d0a0dd447eb0e3524e53300de8f45
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-04 13:19:01 +0600

    Video-number:  [76] Project-Name:  Htmlentities_htmlspeciakchars-Function
               ( hard and confusion  )

---
+++ [76]Htmlentities_htmlspeciakchars-Function/Htmlentities_htmlspeciakchars-Function.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$name="This is my<b>'home'</b>";
+echo htmlentities($name)."<br>";
+
+$name="This a a 'bird' <b>with my home</b>";
+echo htmlentities($name)."<br>";
+
+$name="<a href=https://www.youtube.com/watch?v=oXX2SJKijig&list=PL0b6OzIxLPbyrzCMJOFzLnf_-_5E_dkzs&index=6></a>"; 
+echo htmlentities($name)."<br>";
+
+
+//htmlentities----------
+
+
+$name="<a href=https://www.youtube.com/watch?v=oXX2SJKijig&list=PL0b6OzIxLPbyrzCMJOFzLnf_-_5E_dkzs&index=6></a>"; 
+echo htmlentities($name,ENT_QUOTES)."<br>";
+
+//ENT_QUOTES---------
+
+
+$name="<a href=https://www.youtube.com/watch?v=oXX2SJKijig&list=PL0b6OzIxLPbyrzCMJOFzLnf_-_5E_dkzs&index=6></a>"; 
+echo htmlentities($name,ENT_NOQUOTES)."<br>";
+
+
+//ENT_NOQUOTES--------
+
+
+
+
+
+
+
+
+
+
+?>
