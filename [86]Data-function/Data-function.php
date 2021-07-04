4a06e196339326c0517e24c99dd6733c011139f4
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-04 22:55:23 +0600

    Vedio-Number:  [86] Project-Name:  Data-function

---
+++ [86]Data-function/Data-function.php
@@ -0,0 +1,25 @@
+<?php 
+
+echo "Today is ". date("d")."<br>";//date
+echo "Today is ". date("D") ."<br>";//day
+echo "Today is ".date("js")."<br>";
+echo "Today is ".date("F")."<br>";// capital month
+echo "Today is ".date("M")."<br>";//small month
+echo "Today is ".date("m")."<br>";// date-month
+echo "Today is ".date("d/m/y")."<br>";//full-details
+echo "Today is ".date("d-m-y")."<br>";
+echo "Tpday is ".date("y-d-m")."<br>";
+
+
+echo "Today is ".date("F")."<br>";
+echo "Today is ".date("d")."<br>";
+
+
+
+
+
+
+
+
+
+?>
