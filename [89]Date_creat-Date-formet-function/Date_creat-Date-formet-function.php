2c3bad1d7f869c6e08861c78df93c3b36d97f963
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-05 18:12:33 +0600

    Video-Number:  [89] Project-Name:  Date_creat-Date-formet-function

---
+++ [89]Date_creat-Date-formet-function/Date_creat-Date-formet-function.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+$date=date_create("2021-09-24");
+echo date_format($date,"l")."<br>";
+
+$date=date_create("2001-06-07");
+echo date_format($date,"l")."<br>";
+
+$date=date_create("1998-08-18");
+echo date_format($date,"l"),"<br>";
+
+$date=date_create("2022-03-19");
+echo date_format($date,"l")."<br>";
+
+
+$date=date_create("Asia/Dubai");
+echo date_format($date,"d-m-y");
+
+
+
+
+
