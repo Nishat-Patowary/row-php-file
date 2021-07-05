3a8fdbd9fcaf6459cab136ca3f3baebee72b032e
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-05 17:43:18 +0600

    Video-Number:  [88] Project-Name:  MKtime-Gmmktime-function

---
+++ [88]MKtime-Gmmktime-function/MKtime-Gmmktime-function.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+echo date("l", mktime(0,0,0,6,7,2001))."<br>";
+echo date("l", mktime(0,0,0,8,18,1998))."<br>";
+echo date("l", mktime(0,0,0,6,18,1899))."<br>";
+echo date("l" ,mktime(0,0,0,8,10,2021))."<br>";
+
+
+
+echo date("m-t-d" , mktime(0,0,0,2,23,1010))."<br>";
+echo date("d-m-y", mktime(0,0,0,8,18,1998))."<br>";
+echo date("i:h:g",mktime(0,0,0,4,25,2021))."<br>";
+
+
+
+
+
+
+?>
