f323015121a966b25c17d0d8640b7bedea6f9634
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-07-10 22:21:04 +0600

    Video-Number:  [104] Project-Name:  Die_Edit-function

---
+++ [104]Die_Edit-function/Die_Exit-function.php
@@ -0,0 +1,25 @@
+<?php 
+
+echo "1. This is number<br>";
+echo "2. This is number<br>";
+echo "3. This is number<br>";
+die();
+echo "4. This is number<br>";
+die("There is a erroe");
+echo "5. This is number<br>";
+echo "6. This is number<br>";
+
+//die uesd---------------
+
+echo "This is a number<br>";
+echo "This is a number<br>";
+exit("There is some error");
+echo "This is a number<br>";
+echo "This is a number<br>";
+exit();
+echo "This is a number<br>";
+echo "This is a number<br>";
+
+//exit used----------
+
+?>
